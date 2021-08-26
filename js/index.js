8768e376f6dd7852f18ab726e912036e02466798
estiakahmed <71464257+estiakahmed@users.noreply.github.com>
2021-08-26 15:44:58 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").onclick = function () {
+    location.href = "https://financialsupportonline.co.uk/form/?firstName=Riccardo&lastName=May&phone=07777777778&email=riccardo@monetise.co.uk&source=new-source&clickid=123456";
+};
